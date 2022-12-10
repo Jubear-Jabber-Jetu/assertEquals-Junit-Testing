320bf773aa4b2b9201879128780c666c68174d3c
Jubear Jabber Jetu <115889245+Jubear-Jabber-Jetu@users.noreply.github.com>
2022-12-10 18:16:39 +0600

    Add files via upload

---
+++ testcase.java
@@ -0,0 +1,13 @@
+package one;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class testcase {
+
+@Test
+public void testSetup() {
+   String str= "I am done with Junit setup";
+   assertEquals("I am with Junit setup",str);
+}
+}
